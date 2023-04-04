4b20303609a1dbc7616020de3facff20c08a4d2f
TUNGB1913351 <https://github.com/TUNGB1913351/CT449_LAP2.git>
2023-04-04 08:37:34 +0700

    first commit

---
+++ ReactJS/src/containers/AdminDashboard/layouts/dashboard/SidebarConfigDoctorVi.js
@@ -0,0 +1,27 @@
+// component
+import Iconify from "../../components/Iconify";
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
+
+const SidebarConfigDoctorVi = [
+  {
+    title: "Quản lý bệnh nhân",
+    path: "/admin-dashboard/doctor/manage-patient",
+    icon: getIcon("medical-icon:i-inpatient"),
+  },
+  {
+    title: "Quản lý ca khám bệnh",
+    path: "/admin-dashboard/doctor/manage-schedule-doctor",
+    icon: getIcon("healthicons:i-schedule-school-date-time"),
+  },
+  {
+    title: "Quản lý thuốc",
+    path: "/admin-dashboard/manage-drug",
+    icon: getIcon("mdi:drugs"),
+  },
+
+];
+
+export default SidebarConfigDoctorVi;
